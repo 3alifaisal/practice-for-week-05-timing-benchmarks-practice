b1a34e32d30d6eaff63d84a4faa04072e4e0532e
Ali Avada <110764863+3alifaisal@users.noreply.github.com>
2023-12-15 15:01:39 +0100

    complexity
    Ex easy 20

--- phase-2.js
+++ phase-2.js
@@ -1,15 +1,33 @@
 const [addNums, addManyNums] = require("./phase-1");
 
-// Runs `addNums` in 10 increasing increments
 function addNums10(increment) {
-  // Fill this in
+  const sums = [];
 
+  for (let i = increment; i <= 10 * increment; i += increment) {
+    // Calculate the sum using the formula
+    let sum = (i * (i + 1)) / 2;
+    sums.push(sum);
+  }
+
+  return sums;
 }
+
+
 
 // Runs `addManyNums` in 10 increasing increments
 function addManyNums10(increment) {
-  // Fill this in
+  const sums = [];
 
+
+  for (let i = increment; i <= 10 * increment; i += increment) {
+    let sum = 0;
+    for (let j = 1; j <= i; j++) {
+      sum += addNums(j);
+    }
+    sums.push(sum);
+  }
+
+  return sums;
 }
 
 module.exports = [addNums10, addManyNums10];
--- phase-3.js
+++ phase-3.js
@@ -1,29 +1,61 @@
 const [addNums, addManyNums] = require("./phase-1");
 
 function addNums10Timing(increment) {
-  // Copy your `addNums10` code here
-  // Then, add timing code
+  const sums = [];
 
-  // Your code here
+  for (let i = increment; i <= 10 * increment; i += increment) {
+    // Calculate the sum using the formula
+    let sum = (i * (i + 1)) / 2;
+    sums.push(sum);
+  }
 
+  return sums;
 }
 
 
 function addManyNums10Timing(increment) {
-// Copy your `addManyNums10` code here
-// Then, add timing code
+  const sums = [];
 
-  // Your code here
 
+  for (let i = increment; i <= 10 * increment; i += increment) {
+    let sum = 0;
+    startTime = Date.now()
+    for (let j = 1; j <= i; j++) {
+      
+      sum += addNums10Timing(j);
+     
+
+      
+    } 
+    endTime = Date.now();
+
+    console.log( `${endTime - startTime}`);
+    sums.push(sum);
+  }
+
+  return sums;
 }
 
+// console.time("addNums");
+// addNums10Timing(1000000);
+// console.timeEnd("addNums");
 
-n = 1000000
-console.log(`addNums(${n}): `);
-addNums10Timing(1000000);
 
+
+// n = 1000000
+// console.log(`addNums(${n}): `);
+// startTime = Date.now();
+// addNums10Timing(1000000);
+// endTime = Date.now();
+
+// console.log(`Runtime: ${endTime - startTime}ms`);
 console.log("\n***********\n");
 
 n = 1000
+
 console.log(`addManyNums(${n}): `);
+
 addManyNums10Timing(5000);
+
+
+// console.log(`Runtime: ${endTimeOne - startTimeOne}ms`);
